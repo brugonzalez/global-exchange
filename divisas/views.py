3809457c24435eab7086b94396232e2b3409bd1d
abelsati <abel.noguera@sati.com.py>
2025-09-24 21:34:58 -0300

    - Se agrega modelo MetodoCobro faltante con migraciones - Se corrige validación de cliente en FormularioTransaccion - Implementacion parcial lógica de denominación mínima en frontend JS - Correcion de  cálculos de comisión en transacciones - Mejoras en elmanejo de categorías de cliente por defecto - Se agrega redondeo al múltiplo más cercano para denominaciones

--- divisas/views.py
+++ divisas/views.py
@@ -543,6 +543,7 @@ class APIVistaTasasActuales(TemplateView):
                     'ultima_actualizacion': tasa.fecha_actualizacion.isoformat(),
                     'lugares_decimales': tasa.moneda.lugares_decimales,
                     'moneda_id': moneda.id,
+                    'denominacion_minima': moneda.denominacion_minima if moneda.denominacion_minima else None,
                 })
             else:
                 return JsonResponse({'error': 'No se encontró tasa'}, status=404)
@@ -559,6 +560,7 @@ class APIVistaTasasActuales(TemplateView):
                     'ultima_actualizacion': tasa.fecha_actualizacion.isoformat(),
                     'lugares_decimales': tasa.moneda.lugares_decimales,
                     'moneda_id': moneda.id,
+                    'denominacion_minima': moneda.denominacion_minima if moneda.denominacion_minima else None,
                 })
             else:
                 return JsonResponse({'error': 'No se encontró tasa'}, status=404)
